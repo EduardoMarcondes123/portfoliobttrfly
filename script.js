2b4024c2b93f2df21ae6acc21f4dfdb4d24aa934
EduardoMarcondes123 <134169219+EduardoMarcondes123@users.noreply.github.com>
2026-03-03 01:14:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Seleciona todas as imagens da galeria
+const imagens = document.querySelectorAll('.gallery-item img');
 
+imagens.forEach(img => {
+    img.addEventListener('click', () => {
+        alert("Você clicou na arte: " + img.alt);
+    });
+});
